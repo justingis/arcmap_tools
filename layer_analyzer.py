5604de60dd5337579e60f431027e6c5f496a0956
Justin Hawley <justin@orcagis.com>
2022-06-13 15:42:45 -0500

    added new script file layer_analyzer.py

---
+++ layer_analyzer.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------------
+# Name:        Layer Analyzer
+# Purpose:     Output layer information to an XLSX
+#
+# Author:      Justin Hawley
+#
+# Created:     06/13/2022
+#-------------------------------------------------------------------------------
+
+print('Hello Bash!')
