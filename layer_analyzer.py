bd50edb6061fa9fb6a82a66953d398cb18d1d0cc
Justin Hawley <justin@orcagis.com>
2022-06-13 16:03:48 -0500

    added Layer Analyzer script tool to the toolbox

--- layer_analyzer.py
+++ layer_analyzer.py
@@ -7,4 +7,14 @@
 # Created:     06/13/2022
 #-------------------------------------------------------------------------------
 
-print('Hello Bash!')
+
+# Output layer name (excel sheet name)
+# List layer name, description, credits, visible, scale range, source, Display properties???, 
+#   def query, joins and relates
+# List each field in the layer with Field Properties (Name, Alias, Type, ect)
+# Calc % complete and null or empty values
+
+import arcpy
+
+arcpy.AddMessage('Hello Bash!!')
+
