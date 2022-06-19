2abc92c349ebc8f7a806a8d4e12a2180bbcb9dab
Justin Hawley <justin@orcagis.com>
2022-06-19 18:56:48 -0500

    cleaned requirements in comments at top of script

--- layer_reporter.py
+++ layer_reporter.py
@@ -8,13 +8,11 @@
 #-------------------------------------------------------------------------------
 
 
-# Output layer name (excel sheet name)
-# List layer name, description, credits, visible, scale range, source, Display properties???, 
-#   def query, joins and relates
-# List each field in the layer with Field Properties (Name, Alias, Type, ect)
-# Calc % complete and null or empty values
-# Should be able to add multiple layers to create multiple sheets within an Excel doc
-# Output to txt file or csv??
+# Additional functionality that could be added
+# Has Relates
+# Scale Range
+# Other display properties
+# Allow multiple input layers which could output multiple CSV, TXT or sheets within an XLSX document
 
 import arcpy
 
