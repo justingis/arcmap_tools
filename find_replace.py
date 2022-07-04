35d31f18515ccb81db2ff36032a40161f5b79b47
Justin Hawley <justin@orcagis.com>
2022-07-03 21:00:51 -0500

    added find_replace.py file with boilerplate code

---
+++ find_replace.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        Find & Replace
+# Purpose:     Finds all instances of a text pattern and updates the attribute
+#    table accordingly
+#
+# Author:      Justin Hawley (justin@orcagis.com)
+#
+# Created:     07/03/2022
+#
+# Interpreter: C:\Python27\ArcGIS10.8\python.exe
+#-------------------------------------------------------------------------------
+
+import arcpy
+
+def main():
+    print('Hello Bash!')
+
+if __name__ == '__main__':
+    main()
