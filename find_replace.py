c8e1817bf923d14895ded621bb7fe40363f87115
Justin Hawley <justin@orcagis.com>
2022-07-03 22:47:52 -0500

    added find function (regular expressions)

--- find_replace.py
+++ find_replace.py
@@ -11,6 +11,14 @@
 #-------------------------------------------------------------------------------
 
 import arcpy
+import re
+
+def find(pat, text):
+    match = re.search(pat, text)
+    if match:
+        print(match.group())
+    else:
+        print('not found')
 
 def main():
     #input_layer = arcpy.GetParameter(0)
@@ -18,6 +26,7 @@ def main():
     desc = arcpy.Describe(input_layer)
     field_list = desc.fields
     field_names = [f.name for f in field_list]
+
     with arcpy.da.UpdateCursor(input_layer, field_names) as cursor:
         for row in cursor:
             for field in field_list:
