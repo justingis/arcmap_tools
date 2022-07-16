b1db9d45c8812b60a5cb555c2921d9cb2bb7bb19
Justin Hawley <justin@orcagis.com>
2022-07-15 23:38:11 -0500

    Created basic_erase.py file with boiler plate syntax

---
+++ basic_erase.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        Basic Erase
+# Purpose:     Allows for the same functionality as the built-in Erase tool without
+#              requiring the Advanced license (works with Basic license) 
+#
+# Author:      Justin Hawley (justin@orcagis.com)
+#
+# Created:     07/15/2022
+#
+# Interpreter: C:\Python27\ArcGIS10.8\python.exe
+#-------------------------------------------------------------------------------
+
+def basic_erase():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
