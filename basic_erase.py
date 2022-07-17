6ebd62bdacfea69855e544b44e5517fbf11ca6fa
Justin Hawley <justin@orcagis.com>
2022-07-17 10:37:20 -0500

    Created basic_erase function:

--- basic_erase.py
+++ basic_erase.py
@@ -17,8 +17,18 @@ def basic_erase():
     erase_layer = arcpy.GetParameter(1)
     output_feature_class = arcpy.GetParameter(2)
 
+    # union input_layer and erase_layer
+    union_layer = arcpy.Union_analysis(in_features="{} #;{} #".format(erase_layer, input_layer), out_feature_class=output_feature_class, join_attributes="ALL", cluster_tolerance="", gaps="GAPS")
+    union_layer = arcpy.MakeFeatureLayer_management(union_layer,"union_layer")
+
+    field_name = 'FID_{}'.format(erase_layer)
+    where_clause = '{} > -1'.format(field_name)
+
+    arcpy.SelectLayerByAttribute_management(in_layer_or_view=union_layer, selection_type='NEW_SELECTION', where_clause=where_clause)
+    arcpy.DeleteRows_management(union_layer)
+
 def main():
-    pass
+    basic_erase()
 
 if __name__ == '__main__':
     main()
